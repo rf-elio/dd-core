806d69320b48658d0fc737e9b182fb9d8f958d76
Raoul Yemetio <ry@elio-systems.com>
2020-01-17 20:28:24 +0100

    ProductExportManager created, create ProductExport on plugin install, Controller to show the content of .csv

--- src/ElioFactFinder.php
+++ src/ElioFactFinder.php
@@ -3,9 +3,39 @@
 namespace Elio\ElioFactFinder;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Elio\ElioFactFinder\Service\Export\ProductExportManager;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 
 
 class ElioFactFinder extends Plugin
 {
+    public function install(InstallContext $installContext): void
+    {
+        parent::install($installContext);
 
+        /** @var EntityRepositoryInterface $productExportRepository */
+        $productExportRepository = $this->container->get("product_export.repository");
+
+        /** @var EntityRepositoryInterface $salesChannelDomainRepository */
+        $salesChannelDomainRepository = $this->container->get("sales_channel_domain.repository");
+
+        /** @var EntityRepositoryInterface $productStreamRepository */
+        $productStreamRepository = $this->container->get("product_stream.repository");
+
+        /** @var EntityRepositoryInterface $salesChannelRepository */
+        $salesChannelRepository = $this->container->get("sales_channel.repository");
+
+
+        $productExportManager = new ProductExportManager(
+            $productExportRepository,
+            $salesChannelDomainRepository,
+            $productStreamRepository,
+            $salesChannelRepository
+        );
+
+        $productExportManager->installExports();
+
+
+    }
 }
