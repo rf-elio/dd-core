aa3e6ebd9b1ec537fe68382cddb64582a46edf84
Andrey Baev <anb@elio-systems.com>
2021-09-14 20:46:33 +0300

    filling filters from database and refactored drag&drop

--- src/Resources/app/administration/src/module/elio-fact-finder-restrictions/component/elio-fact-finder-restrictions-ruler/index.js
+++ src/Resources/app/administration/src/module/elio-fact-finder-restrictions/component/elio-fact-finder-restrictions-ruler/index.js
@@ -1,8 +1,8 @@
 import template from './elio-fact-finder-restriction-ruler.html.twig';
 import '../../page/ff-restrictions-index/ff-restrictions-index.scss';
 
-const { Component, Mixin } = Shopware;
-const { Criteria } = Shopware.Data;
+const {Component, Mixin} = Shopware;
+const {Criteria} = Shopware.Data;
 
 Shopware.Component.register('ff-restriction-ruler', {
     template: template,
@@ -33,20 +33,28 @@ Shopware.Component.register('ff-restriction-ruler', {
     },
 
     computed: {
-        isLoading() {
-            return false;
-        },
         allowAllChecked() {
             return false;
         },
         blockAllChecked() {
             return false;
+        },
+        filterRepository() {
+            return this.repositoryFactory.create('elio_ff_filter');
+        },
+        filterRestrictionRepository() {
+            return this.repositoryFactory.create('elio_ff_filter_restrictions');
         }
     },
 
     data() {
         return {
-            currentDragItem: null
+            isLoading: false,
+            currentDragItem: null,
+            movedFiltersIds: [],
+            allList: [],
+            allowList: [],
+            blockList: []
         }
     },
 
@@ -56,7 +64,7 @@ Shopware.Component.register('ff-restriction-ruler', {
 
     methods: {
         onCreated() {
-
+            this.loadFilters();
         },
 
         onDragStart(dragData) {
@@ -68,8 +76,17 @@ Shopware.Component.register('ff-restriction-ruler', {
         onDrop(dragData) {
             if (dragData.target.classList.contains("ruler-tab-filter-list")) {
                 dragData.preventDefault();
-                this.currentDragItem.parentNode.removeChild(this.currentDragItem);
-                dragData.target.appendChild(this.currentDragItem);
+                var operator = this;
+                var draggedId = this.currentDragItem.getAttribute("data-filter-id");
+                var targetColumnType = dragData.target.getAttribute("data-filter-column");
+                var fromColumnType = this.currentDragItem.parentNode.getAttribute("data-filter-column");
+
+                this[fromColumnType + 'List'].forEach(function (item, i, arr) {
+                    if (item.id === draggedId) {
+                        operator[targetColumnType + 'List'].push(item);
+                        operator[fromColumnType + 'List'].splice(i, 1);
+                    }
+                });
             }
         },
 
@@ -87,6 +104,77 @@ Shopware.Component.register('ff-restriction-ruler', {
 
         onBlockSelectedClicked() {
             console.log('onBlockSelectedClicked');
+        },
+
+        async loadFilters() {
+            this.isLoading = true;
+            try {
+                var criteria = new Criteria();
+                criteria.addAssociation('filters')
+                if (this.isCategory && this.category_id != null) {
+                    criteria.addFilter(
+                        Criteria.equals('elio_ff_filter_restrictions.isCategory', true)
+                    );
+                    criteria.addFilter(
+                        Criteria.equals('elio_ff_filter_restrictions.categoryId', this.category_id)
+                    );
+                } else {
+                    criteria.addFilter(
+                        Criteria.equals('elio_ff_filter_restrictions.isCategory', false)
+                    );
+                    criteria.addFilter(
+                        Criteria.equals('elio_ff_filter_restrictions.layer', this.layer)
+                    );
+                }
+
+                this.movedFiltersIds = [];
+                var operator = this;
+                await this.filterRestrictionRepository
+                    .search(criteria, Shopware.Context.api)
+                    .then(filterRestrictions => {
+                        filterRestrictions.forEach(function (restrictionColumn) {
+                            restrictionColumn.filters.forEach(function (filter) {
+                                operator.movedFiltersIds.push(filter.id);
+                                if (restrictionColumn.isAllowed) {
+                                    operator.allowList.push(filter);
+                                } else {
+                                    operator.blockList.push(filter);
+                                }
+                            });
+                        });
+                    });
+
+                await this.buildFilters();
+                this.isLoading = false;
+            } catch {
+                this.isLoading = false;
+                this.$router.push({name: 'elio.factfinder.restrictions.index.global'});
+            }
+        },
+
+        async buildFilters() {
+            var criteria = new Criteria();
+            criteria.setLimit(500); /* upddate it */
+            if (this.movedFiltersIds.length > 0) {
+                criteria.addFilter(Criteria.not(
+                    'AND',
+                    [
+                        Criteria.equalsAny('elio_ff_filter.id', this.movedFiltersIds)
+                    ]
+                ));
+            }
+            var operator = this;
+            await this.filterRepository
+                .search(criteria, Shopware.Context.api)
+                .then(filters => {
+                    filters.forEach(function (filter) {
+                        operator.allList.push(filter);
+                    });
+                });
+        },
+
+        save() {
+
         }
     }
 });
