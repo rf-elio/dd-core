4c3a6930a8f7f3af604faa9e8049bd0fbd0a9d77
Andrey Baev <anb@elio-systems.com>
2021-11-22 20:14:23 +0300

    fixes

--- src/Core/FilterRestrictions/FilterRestrictionsDefinition.php
+++ src/Core/FilterRestrictions/FilterRestrictionsDefinition.php
@@ -42,7 +42,6 @@ use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToManyAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToOneAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 use Shopware\Core\System\SalesChannel\SalesChannelDefinition;
