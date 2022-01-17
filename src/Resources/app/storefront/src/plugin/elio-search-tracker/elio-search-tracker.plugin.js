7ef722e0023639247e7aa14f57dafa130ea6fe12
MilicaTadic <mt@elio-systems.com>
2022-01-17 11:44:11 +0100

    Save search history for each sales channel and remove search page urls from local storage

--- src/Resources/app/storefront/src/plugin/elio-search-tracker/elio-search-tracker.plugin.js
+++ src/Resources/app/storefront/src/plugin/elio-search-tracker/elio-search-tracker.plugin.js
@@ -6,44 +6,50 @@ export default class ElioSearchTrackerPlugin extends Plugin {
     };
 
     init() {
-        let searchTerm = this._getSearchTerm();
+        let searchTermObject = this._getSearchTermObject();
 
-        if (searchTerm) {
-            this._storeSearchTerm(searchTerm);
+        if (searchTermObject) {
+            let { channel, searchTerm } = searchTermObject;
+            this._storeSearchTerm(searchTerm, channel);
         }
     }
 
-    _getSearchTerm() {
-        let searchTerm = {};
+    _getSearchTermObject() {
+        let searchTermObject = {};
         let options = this.options;
         
         if (options) {
-            searchTerm = {
+            searchTermObject = {
                 searchTerm: options.searchTerm && options.searchTerm.toLowerCase(),
-                searchPageUrl: options.searchPageUrl
+                channel: options.channel
             };
         }
 
-        return searchTerm;
+        return searchTermObject;
     }
 
-    _storeSearchTerm(searchTerm) {
-        let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    _storeSearchTerm(searchTerm, channel) {
+        if (channel) {
+            let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || {};
 
-        let searchedBefore = searchHistory.some(oldSearchTerm => this._areSearchTermsEqual(oldSearchTerm, searchTerm));
-        if (searchedBefore)
-            searchHistory = searchHistory.filter(oldSearchTerm => !this._areSearchTermsEqual(oldSearchTerm, searchTerm));
+            let sHistoryForChannel = searchHistory[channel] || [];
+            let searchedBefore = sHistoryForChannel.some(oldSearchTerm => this._areSearchTermsEqual(oldSearchTerm, searchTerm));
+            if (searchedBefore)
+                sHistoryForChannel = sHistoryForChannel.filter(oldSearchTerm => !this._areSearchTermsEqual(oldSearchTerm, searchTerm));
 
-        searchHistory.push(searchTerm);
-        let searchHistoryLength = searchHistory.length;
-        let maxLength = this.options.maxHistoryLength;
-        if (searchHistoryLength > maxLength) {
-            searchHistory = searchHistory.slice(searchHistoryLength-maxLength, searchHistoryLength);
+            sHistoryForChannel.push(searchTerm);
+            let searchHistoryLength = sHistoryForChannel.length;
+            let maxLength = this.options.maxHistoryLength;
+            if (searchHistoryLength > maxLength) {
+                sHistoryForChannel = sHistoryForChannel.slice(searchHistoryLength-maxLength, searchHistoryLength);
+            }
+            searchHistory[channel] = sHistoryForChannel;
+
+            localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
         }
-        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     }
 
     _areSearchTermsEqual(term1, term2) {
-        return term1.searchTerm == term2.searchTerm;
+        return term1 == term2;
     }
 }
